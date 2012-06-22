0a9897e608e3f143cae37a5fb5356c01af45be2f
Gordon Dickens <gordon@gordondickens.com>
2012-06-22 13:22:52 -0400

    added old config files, trying to resolve blueprint issues

---
+++ db/h2-non-std-repo.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# JARS=`find $HOME/.m2 -iname "com.springsource.org.h2*"`
+JARS=`find /Volumes/FunfHundert/mavenrepo -iname "com.springsource.org.h2*"`
+
+for JAR in $JARS
+do
+	CLASSPATH=$CLASSPATH:$JAR
+done
+
+if [ -z $CLASSPATH ]
+then
+	echo "Cannot find H2 jars in .m2 Maven repository"
+	exit -1
+fi
